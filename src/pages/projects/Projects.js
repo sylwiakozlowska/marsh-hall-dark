a0719e859c6c48f3e5f2b3c440abee73d2975a18
Sylwia Kozlowska <kozlowska.sylwia90@gmail.com>
2020-06-10 14:14:02 +0100

    add font-family

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -3,7 +3,7 @@ import React from "react";
 function Projects(){
     return (
         <div className="projects-component page">
-            <h1>Coming soon . . .</h1>
+            <h1 className="title is-1">Coming soon . . .</h1>
         </div>
     )
 }
