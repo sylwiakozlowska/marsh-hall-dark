15c78b8f99ec06e565765fab811047240f3857cf
Sylwia Kozlowska <kozlowska.sylwia90@gmail.com>
2020-06-13 11:58:49 +0100

    Updates

---
+++ precache-manifest.afdc97b2a9189ae02f3abff47896fec3.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56084fe7eef7e3e851df9d6ba9a63a4",
+    "url": "/marsh-hall-dark/index.html"
+  },
+  {
+    "revision": "3c045ba271387bdeef03",
+    "url": "/marsh-hall-dark/static/css/2.aa407cf3.chunk.css"
+  },
+  {
+    "revision": "da1eb553518746b08de3",
+    "url": "/marsh-hall-dark/static/css/main.d036de50.chunk.css"
+  },
+  {
+    "revision": "3c045ba271387bdeef03",
+    "url": "/marsh-hall-dark/static/js/2.4fcb50df.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/marsh-hall-dark/static/js/2.4fcb50df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da1eb553518746b08de3",
+    "url": "/marsh-hall-dark/static/js/main.8cf914d0.chunk.js"
+  },
+  {
+    "revision": "388baabaa250259bcf29",
+    "url": "/marsh-hall-dark/static/js/runtime-main.07620883.js"
+  },
+  {
+    "revision": "7074597347e3d802e869cb69602a1657",
+    "url": "/marsh-hall-dark/static/media/Nunito-Bold.70745973.ttf"
+  },
+  {
+    "revision": "be78e14e73ff5f2c46457ad6018f9c3b",
+    "url": "/marsh-hall-dark/static/media/Nunito-Light.be78e14e.ttf"
+  },
+  {
+    "revision": "cb5e3cea83701793d92da3cc4ba42fb0",
+    "url": "/marsh-hall-dark/static/media/colorconsultation_480by360.cb5e3cea.jpg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/marsh-hall-dark/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/marsh-hall-dark/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/marsh-hall-dark/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/marsh-hall-dark/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/marsh-hall-dark/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/marsh-hall-dark/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/marsh-hall-dark/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/marsh-hall-dark/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/marsh-hall-dark/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/marsh-hall-dark/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "c14bc462590e7f500069086c5a93f682",
+    "url": "/marsh-hall-dark/static/media/floor1_480by360.c14bc462.jpg"
+  },
+  {
+    "revision": "3e9237a4ebea001220a04ef56a9f40b2",
+    "url": "/marsh-hall-dark/static/media/home640by480.3e9237a4.jpg"
+  },
+  {
+    "revision": "32e6ee08b1bb19baeb1da9ee0da718b0",
+    "url": "/marsh-hall-dark/static/media/icon.32e6ee08.svg"
+  },
+  {
+    "revision": "e01bf756f049c72f7a5794e234f4a0d2",
+    "url": "/marsh-hall-dark/static/media/interior_480by360.e01bf756.jpg"
+  },
+  {
+    "revision": "101bbef70ee7737726d25d3117d942fb",
+    "url": "/marsh-hall-dark/static/media/logo.101bbef7.png"
+  },
+  {
+    "revision": "6cb2dfe978b705dcd51ce9bb4d0dcbec",
+    "url": "/marsh-hall-dark/static/media/price1_1132by566.6cb2dfe9.jpg"
+  },
+  {
+    "revision": "4a80b972482413c4885e60b132469d86",
+    "url": "/marsh-hall-dark/static/media/profile1_800by600.4a80b972.jpg"
+  },
+  {
+    "revision": "1590185d92c47bae6cd972448fe11b77",
+    "url": "/marsh-hall-dark/static/media/profile2_800by600.1590185d.jpg"
+  },
+  {
+    "revision": "9a0c99d3537b25f2d9cfd1f9b36ddc5f",
+    "url": "/marsh-hall-dark/static/media/profile3_800by600.9a0c99d3.jpg"
+  },
+  {
+    "revision": "2221277322c2c139644353ade29e9162",
+    "url": "/marsh-hall-dark/static/media/roof5_480by360.22212773.jpg"
+  },
+  {
+    "revision": "f1c35c8d580e10683bc6719d4a249807",
+    "url": "/marsh-hall-dark/static/media/wallpaper1_480by360.f1c35c8d.jpg"
+  },
+  {
+    "revision": "8d39835e3f957dd8e7ea1586b7bc36b2",
+    "url": "/marsh-hall-dark/static/media/window1_480by360.8d39835e.jpg"
+  }
+]);
